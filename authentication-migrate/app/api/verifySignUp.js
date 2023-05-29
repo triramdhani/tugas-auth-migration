37e0991cd3f9d634d2a5c729dd30a07f673cddc2
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-05-29 07:40:37 +0700

    (+)

---
+++ authentication-migrate/app/api/verifySignUp.js
@@ -0,0 +1,58 @@
+const User = require('../models').User
+const config = require('../config/configRoles.js');
+const ROLEs = config.ROLEs;
+
+const checkDuplicateUserNameOrEmail = (req, res, next) => {
+    User.findOne({
+        where: {
+            id: req.body.id
+        }
+    }).then(user => {
+        if (user) {
+            res.status(400).send({
+                auth: false,
+                id: req.body.id,
+                message: "Error",
+                errors: "Id is already taken!"
+            });
+            return;
+        }
+
+        User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(user => {
+            if (user) {
+                res.status(400).send({
+                    auth: false,
+                    id: req.body.id,
+                    message: "Error",
+                    errors: "Email is already taken!"
+                });
+                return;
+            }
+            next();
+        });
+    });
+}
+
+const checkRolesExisted = (req, res, next) => {
+    for (let i = 0; i < req.body.roles.length; i++) {
+        if (!ROLEs.includes(req.body.roles[i].toUpperCase())) {
+            res.status(400).send({
+                auth: false,
+                id: req.body.id,
+                message: "Error",
+                errors: "Does NOT exist Role = " + req.body.roles[i]
+            });
+            return;
+        }
+    }
+    next();
+}
+
+module.exports = {
+    checkDuplicateUserNameOrEmail,
+    checkRolesExisted
+}
